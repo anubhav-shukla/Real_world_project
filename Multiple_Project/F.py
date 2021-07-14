c904d64e53042aee8ff08a8478b10ce82f285fc0
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2021-07-14 20:42:01 +0530

    multiple Project
    Here in this folder we make multiple project that help you ...
    
     First project is Catepiller : --- It is a project like snake game you run a snake and you than You get some score..
    
    Hope you play and enjoy this game .....
    
    Second project is Defang ip : ---- It is a great question during interview ..
                                                         when interviewer ask you what is this .....
    
    Here you gave an ip after you got the . as like [.]   and it is called defang  ip/...
    
    Next project is F.py :---- Here you write your name and show in a great format ...\
                             show your name.................
    
    Next project is Shutdown:-------------- If you run this program than your system is shutdown.....'
    
    
    
    Hope you run all these project in your IDE and you enjoy it............
    
    Thankyou...

---
+++ Multiple_Project/F.py
@@ -0,0 +1,3 @@
+import pyfiglet
+font = pyfiglet.figlet_format('Anubhav shukla')
+print(font)
---
+++ Multiple_Project/defang_ip.py
@@ -0,0 +1,8 @@
+def ip_address(address):
+    new_address = ""
+    split_address = address.split(".")
+    separator = "[.]"
+    new_address = separator.join(split_address)
+    return new_address
+ipaddress = ip_address("120.21.003")
+print(ipaddress)
